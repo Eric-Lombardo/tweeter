087d10fef414a566e1a6c7204721c84adf174502
Eric-Lombardo <ejlanim8or@gmail.com>
2020-02-25 23:54:17 +0000

    add tweets dynamically to html body given a data obj

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,7 +5,7 @@
  */
 
 // temporary data
-const tweetData = {
+const tweetObj = {
   "user": {
     "name": "Newton",
     "avatars": "https://i.imgur.com/73hZDYK.png",
@@ -19,8 +19,45 @@ const tweetData = {
 
 
 
-const createTweetElement = function(tweetObj) {
-  const $tweet = $("<article>").addClass("old-tweets")
 
-  const $header = $("<header>").addClass("user")
+
+const createTweetElement = function(tweetObj) {
+  const $tweet = $("<article>").addClass("tweet")
+  
+  const $headerUser = $("<div>").addClass("user");
+  const $userImg = $("<img>").attr("src", tweetObj.user.avatars)
+  const $userName = $("<p>").text(tweetObj.user.name)
+  $headerUser.append($userImg);
+  $headerUser.append($userName);
+  const $userTag = $("<p>").text(tweetObj.user.handle)
+  const $header = $("<header>").append($headerUser)
+  $header.append($userTag);
+  
+  const $tweetContent = $("<p>").text(tweetObj.content.text);
+  const $hr = $("<hr>");
+  
+  const $iconGroup = $("<div>").addClass("icon-group");
+  const $flag = $("<i>").addClass("fas fa-flag");
+  const $sync = $("<i>").addClass("fas fa-sync");
+  const $heart = $("<i>").addClass("fas fa-heart");
+  $iconGroup.append($flag);
+  $iconGroup.append($sync);
+  $iconGroup.append($heart);
+  const $created = $("<p>").text(tweetObj.created_at);
+  const $footer = $("<footer>").append($created)
+  $footer.append($iconGroup);
+  
+  $tweet.append($header)
+  $tweet.append($tweetContent)
+  $tweet.append($hr)
+  $tweet.append($footer)
+  
+  return $tweet;
 }
+
+$(document).ready(function() {
+  const $newTweet = createTweetElement(tweetObj);
+  console.log($newTweet);
+  console.log($("#tweets-container"));
+  $("#tweets-container").append($newTweet);
+})
--- public/scripts/get-time.js
+++ public/scripts/get-time.js
@@ -2,5 +2,5 @@ let time = new Date;
 let timeString = time.toDateString().substring(4,15);
 
 $(document).ready(function() {
-  $("#one footer > p").text(timeString)
+  $(".tweet footer > p").text(timeString)
 })
