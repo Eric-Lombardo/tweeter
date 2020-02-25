265a7b496bd8c9d7b7c56d09c7f8dda53b21cb6a
Eric-Lombardo <ejlanim8or@gmail.com>
2020-02-25 19:49:42 +0000

    allow backspace key to be registered for counting character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,31 @@
+let counter = 0;
+let maxChar = 140;
+
 $("#newTweetText").on("keypress", function(event) {
   // getting text length from textarea input
-  let counter = $(`#${this.id}`)[0].textLength + 1;
+  counter = $(`#${this.id}`)[0].textLength + 1;
 
   // targetting element .counter to update number
-  $(".counter")[0].innerHTML = 140 - counter;
+  $(".counter")[0].innerHTML = maxChar - counter;
 
-  if (140 - counter < 0) {
+  // if negative add class
+  if (maxChar - counter < 0) {
     $(".counter").first().addClass("gone-over-limit")
+  }
+})
+
+
+// if there's a backspace pressed adjust count
+$("#newTweetText").on("keydown", function(event) {
+  if (event.keyCode === 8) {
+    counter--;
+    if (counter <= 0) {
+      counter = 0;
+    }
+    $(".counter")[0].innerHTML = maxChar - counter; 
+  }
+
+  if (maxChar - counter >= 0) {
+    $(".counter").first().removeClass("gone-over-limit")
   }
 })
