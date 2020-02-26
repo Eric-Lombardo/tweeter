cf8d050ae0e0529be714c1f450b03af53e9e7aeb
Eric-Lombardo <ejlanim8or@gmail.com>
2020-02-26 19:07:00 +0000

    add ajax GET to tweet button to add tweet to DB

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,30 @@
  */
 
 // temporary data
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 
 // create html element with dataobj
@@ -77,9 +77,9 @@ const renderTweets = function(dataArr) {
   }
 }
 
-$(document).ready(function() {
-  renderTweets(data);
-})
+// $(document).ready(function() {
+//   renderTweets(data);
+// })
 
 // ---------------- building elements above -------------
 // ---------------- ajax post request below -------------
@@ -90,8 +90,8 @@ const loadTweets = function() {
     url: "/tweets"
   })
     .then(function(data) {
-      console.log(data);
-      return data;
+      console.log(Array.isArray(data));
+      return renderTweets(data);
     })
 }
 
@@ -108,10 +108,8 @@ $(document).ready(function() {
       url: "/tweets",
       data: formData
     })
-      .done(function(message) {
-      })
+      .done(() => loadTweets());
     
-    loadTweets();
   })
 })
 
