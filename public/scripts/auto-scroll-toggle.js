31c35ce1ff0617817d11eda698bb62b49d7e5ee2
Eric-Lombardo <ejlanim8or@gmail.com>
2020-02-27 03:08:07 +0000

    add all expected auto-scroll features

--- public/scripts/auto-scroll-toggle.js
+++ public/scripts/auto-scroll-toggle.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
   
+  $("#auto-scroll").hide();
+
+
+  // fading in/out depending on window scroll value
   $(window).scroll(function() {
     if ($(this).scrollTop() > 400) {
       $(".navbar-right-group").fadeOut();
+      $("#auto-scroll").fadeIn();
     } else {
       $(".navbar-right-group").fadeIn();
+      $("#auto-scroll").fadeOut();
     }
+  })
+
+  // click features for #auto-scroll
+  $("#auto-scroll").on("click", function() {
+    $(window).scrollTop(0);
+    $(".new-tweet").slideDown("slow");
+    $("#newTweetText").focus()
   })
 
 })
